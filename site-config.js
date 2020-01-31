eba28ab98363535f1b4b0b067f945707a81a8808
barry huang <barry.huang93@gmail.com>
2020-01-31 16:09:28 -0500

    site details updated

--- site-config.js
+++ site-config.js
@@ -2,15 +2,14 @@ const path = require('path');
 
 module.exports = {
   siteTitle: 'Barry Huang',
-  siteTitleShort: 'GatsbyU',
-  siteDescription: 'An opinionated starter for Gatsby.',
-  siteUrl: 'https://gu.fabianschultz.com',
+  siteTitleShort: 'BarryH',
+  siteDescription: "Barry Huang's Portfolio Site",
+  siteUrl: 'https://barry.codes',
   themeColor: '#000',
   backgroundColor: '#fff',
   pathPrefix: null,
   logo: path.resolve(__dirname, 'src/images/icon.png'),
   social: {
-    twitter: 'gatsbyjs',
-    fbAppId: '966242223397117',
+    twitter: 'barryydalive',
   },
 };
