5143814acbcd61bb550271b79cde749bc497a226
barry huang <barry.huang93@gmail.com>
2020-01-31 19:36:48 -0500

    undo last commit

--- src/global.css.js
+++ src/global.css.js
@@ -21,7 +21,7 @@ export default createGlobalStyle`
     font-size: 100%;
     font: inherit;
     vertical-align: baseline;
-    overflow: hidden;
+    // overflow: hidden;
   }
 
   /* HTML5 display-role reset for older browsers */
