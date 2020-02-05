7fb0c6023abcc8e28b39a4c9e0419efa388ef39d
barry huang <barry.huang93@gmail.com>
2020-02-05 15:20:21 -0500

    animation slowed

--- src/components/box/box.css.js
+++ src/components/box/box.css.js
@@ -1,18 +1,29 @@
 import styled from 'styled-components';
-import posed from 'react-pose'
+import posed from 'react-pose';
 export const Container = styled.div`
   padding: 2rem 4rem 0 4rem;
   max-width: 60em;
   margin-bottom: 5vh;
-  text-align: center
-  `;
+  text-align: center;
+`;
 
 const spinningDiv = posed.div({
   hoverable: true,
-  init: {rotateY:'0deg'},
-  hover: {rotateY: '180deg'},
-
-})
+  init: {
+    rotateY: '0deg',
+    transition: {
+      duration: 400,
+      ease: 'easeOut',
+    },
+  },
+  hover: {
+    rotateY: '180deg',
+    transition: {
+      duration: 500,
+      ease: 'easeIn',
+    },
+  },
+});
 
 export const ImageContainer = styled(spinningDiv)`
   min-width 100px;
@@ -23,10 +34,10 @@ export const ImageContainer = styled(spinningDiv)`
     border-radius:50%;
     filter:grayscale(10%) contrast(100%);
   };
-`
+`;
 export const FlexBox = styled.div`
   display: flex;
   width: 100vw;
   flex-direction: column;
   align-items: center;
-`
+`;
